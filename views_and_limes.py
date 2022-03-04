ec37a062471cea14cb8c148a4c84fccc115eccf9
K.C <kmacallaghan@gmail.com>
2022-03-04 09:09:06 -0500

    Update views_and_limes.py

--- views_and_limes.py
+++ views_and_limes.py
@@ -1,4 +1,3 @@
-import codecademylib
 from matplotlib import pyplot as plt
 
 months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
@@ -11,7 +10,7 @@ persian_limes_per_month = [67.0, 51.0, 57.0, 54.0, 83.0, 90.0, 52.0, 63.0, 51.0,
 blood_limes_per_month = [75.0, 75.0, 76.0, 71.0, 74.0, 77.0, 69.0, 80.0, 63.0, 69.0, 73.0, 82.0]
 
 
-# create your figure here
+# creating figure 1 here
 plt.figure(figsize=(12, 8))
 ax1 = plt.subplot(1, 2, 1)  # left subplot
 x_values = range(len(months))
@@ -22,6 +21,7 @@ plt.ylabel('Page Visits')
 ax1.set_xticks(x_values) 
 ax1.set_xticklabels(months)
 
+# creating figure 2 here
 ax2 = plt.subplot(1, 2, 2) # right subplot
 plt.plot(x_values, key_limes_per_month, color='lightgreen')
 plt.plot(x_values, persian_limes_per_month, color='green')
